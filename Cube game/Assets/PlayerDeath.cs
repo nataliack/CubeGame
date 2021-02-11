ae2b13a3a2386fbfe6f975266b006bd1b18b1c6b
Natalia Chamon <nataliachamon@Natalias-MacBook-Pro-2.local>
2021-02-11 11:56:24 +1000

    Cube Game

---
+++ Cube game/Assets/PlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerDeath : MonoBehaviour
+{
+    private void Start()
+    {
+       
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+}
